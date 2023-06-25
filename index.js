0257b0aff12e04242b7b7ca2bce5de4242b49557
luoqiming82 <qimingluo28@gmail.com>
2023-06-25 14:46:49 +0800

    finished at replit, copy to local

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
-
+app.use(require('morgan')('dev'));
 app.use(cors());
+//body-parser
+app.use(express.urlencoded({extended: true}));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -18,6 +20,10 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+// api router
+const shortenUrlApi = require('./shortUrl.js');
+app.use('/api', shortenUrlApi);
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
---
+++ shortUrl.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const router = express.Router();
+//import valide-url
+const validUrl = require('valid-url')
+//import replit db
+const Database = require("@replit/database"); 
+const db = new Database();
+
+/* url will be store as shortUrl: originalUrl 
+shortUrl will be 's'+rendom number
+each shortUrl will be unique
+different shortUrl can share same originalUrl, because different users could shorten the same original url
+*/
+
+//define verify-url fn
+const verifyUrl = function(url){
+  if(validUrl.isWebUri(url)){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+// parent path: '/api'
+// store urls 
+router.post('/shorturl', async function(req, res){
+  const originalUrl = req.body.url;
+  // step 1: verify url
+  const validOriginalUrl = verifyUrl(originalUrl);
+  // step 2: 
+  if(!validOriginalUrl){
+    res.status(400).json({error: 'invalid url'});
+  }else{
+    // extract all shortUrls stored in db
+    const shortUrls = await db.list(); // return an array
+    // shorten url &&  make sure each shortUrl will be unique
+    let shortUrl = 's' + Math.floor(Math.random() * 10000); // db can store 9999 shorturl
+    while(shortUrls.includes(shortUrl)){
+      shortUrl = 's' + Math.floor(Math.random() * 10000); // db can store 9999 shorturl
+    }
+    // store urls in db
+    await db.set(shortUrl, originalUrl);
+    // send back info
+    res.json({
+      original_url: originalUrl,
+      short_url: shortUrl
+    })
+  }
+  
+});
+
+// redirect url
+router.get('/shorturl/:shortUrl', async function(req, res){
+  const shortUrlInputed = req.params.shortUrl;
+  // get originalUrl
+  const originalUrl = await db.get(shortUrlInputed);
+  if(!originalUrl){
+    res.status(400).json({error: 'no such short_url'});
+  }else{
+    res.redirect(originalUrl);
+  }
+})
+
+module.exports = router;
+
